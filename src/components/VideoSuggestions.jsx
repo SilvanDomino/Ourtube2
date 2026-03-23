d2bb4c5f75ec21b1a32b1a08b38d1149fb5c70bf
39613 <39613@ma-web.nl>
2026-03-23 12:42:49 +0100

    show info af

--- src/components/VideoSuggestions.jsx
+++ src/components/VideoSuggestions.jsx
@@ -3,16 +3,30 @@ import styles from "./videoSuggestions.module.css";
 export function VideoSuggestions() {
   const video_info = [
     {
-      url: "https://i.ytimg.com/vi/xJ9oQKMZOh4/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBcIuIb-RAAx351tb7M9i1cRQ4w5w",
-      title: "SIDEMEN CHARITY MATCH 2026: THE DRAFT",
+      url: "https://i.ytimg.com/vi/KCMcS7cYYgU/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBV7yAA5g7J8Vilch6ZNnuEYinoIQ",
+      title: "We Gave Yonna A Tour of The OSCS House!",
+      creator: "Yugi2x Live",
+      duur: "16:48",
     },
   ];
+
   const suggestion = video_info.map((item) => (
-    <div className={styles.container}>
-      <img src={item.url} alt="" />
-      <p>{item.title}</p>
+    <div className={styles.videoSuggestion}>
+      <img src={item.url} className={styles.videoImg} />
+      <div className={styles.videoDescription}>
+        <div className={styles.videoTitle}>{item.title}</div>
+        <div className={styles.videoUploader}>{item.creator}</div>
+        <div className={styles.videoLength}>{item.duur}</div>
+      </div>
     </div>
   ));
 
-  return <div className={styles.videoSuggestions}>{suggestion}</div>;
+  return (
+    <div className={styles.videoSuggestions}>
+      {suggestion}
+      {suggestion}
+      {suggestion}
+      {suggestion}
+    </div>
+  );
 }
