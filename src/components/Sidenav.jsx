137e3038cfda95351a90102e036644995291797c
jessica-vdz <jessicavdz02@gmail.com>
2026-03-23 12:40:55 +0100

    added functionality

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,9 +1,29 @@
 import styles from './sidenav.module.css';
+import { useState } from "react";
 
 export function Sidenav() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
   return (
     <nav className={styles.sidenav}>
-      Sidenav Component
+      <div className={styles.navigation}>
+
+        <button className={styles.hamburger} onClick={toggleHamburger}>X</button>
+
+        <ul className={`${styles.menu} ${hamburgerOpen ? styles.show : ''}`}>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+        </ul>
+
+      </div>
     </nav>
   );
-};
+}
