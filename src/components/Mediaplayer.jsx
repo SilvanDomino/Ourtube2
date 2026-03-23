0c568d3e45087ad3a3ddd2608598707c6a27d085
Nick <nick.esselman@gmail.com>
2026-03-23 12:01:06 +0100

    just copypaseed silvan XD

--- src/components/Mediaplayer.jsx
+++ src/components/Mediaplayer.jsx
@@ -4,12 +4,34 @@ import video1 from '../assets/NGYU.mp4';
 import { useRef, useState } from 'react';
 
 export function Mediaplayer() {
-
   const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [status, setStatus] = useState(0);
 
+  function togglePlay(){
+    console.log("play/pause");
+    if(isPlaying){
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+    
+  }
   return (
-    <div ref={videoRef} className={styles.mediaplayer}>
-      <video className={styles.video} src={video1} controls></video>
+    <div className={styles.mediaplayer}>
+      <video ref={videoRef} className={styles.video} src={video1}></video>
+      <div className={styles.controlContainer}>
+        <div className={styles.controls}>
+          <button>prev</button>
+          <button className={styles.playButton} onClick={togglePlay}>
+            <div className={styles.buttonContent}>
+              {!isPlaying? "▶︎" : "❚❚"}
+            </div>
+          </button>
+          <button>next</button>
+        </div>
+      </div>
     </div>
   );
 };
