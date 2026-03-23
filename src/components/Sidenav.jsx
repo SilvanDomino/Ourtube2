7209686e31f1725e8677a7e97395a9f83f447df8
Kurt Leenen <39231@ma-web.nl>
2026-03-23 12:44:38 +0100

    sidenav section finish

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -19,25 +19,21 @@ export function Sidenav() {
         </div>
         <div className={styles.sideSection} id="3">
           <h3>Explore</h3>
-          <Link to="/">Music</Link>
-          <Link to="/Short">Movies</Link>
-          <Link to="/Subbed">Gaming</Link>
+          <Link to="/Msc">Music</Link>
+          <Link to="/Mvs">Movies</Link>
+          <Link to="/Gmg">Gaming</Link>
           <Link to="/Nws">News</Link>
           <Link to="/Spt">Sport</Link>
           <Link to="/Pdcst">Podcast</Link>
         </div>
         <div className={styles.sideSection} id="4">
-          <h3>
-            More from OurTube
-          </h3>
-          <Link to="/">Home</Link>
-          <Link to="/Short">Shorts</Link>
-          <Link to="/Subbed">Subscriptions</Link>
+          <h3>More from OurTube</h3>
+          <Link to="/Abo/Prm">OurTube Premium</Link>
+          <Link to="/Abo/Msc">OurTube Music</Link>
+          <Link to="/Abo/Kids">OurTube Kids</Link>
         </div>
         <div className={styles.sideSection} id="5">
-          <Link to="/">Home</Link>
-          <Link to="/Short">Shorts</Link>
-          <Link to="/Subbed">Subscriptions</Link>
+          <Link to="/RptHs">Report History</Link>
         </div>
       </BrowserRouter>
     </nav>
