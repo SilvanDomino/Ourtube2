ab5aa2e2edc6d964d9894355077514d1c01cb5b2
Nick <nick.esselman@gmail.com>
2026-03-23 11:59:30 +0100

    added videoref

--- src/components/Mediaplayer.jsx
+++ src/components/Mediaplayer.jsx
@@ -1,9 +1,14 @@
 import styles from './mediaplayer.module.css';
 import video1 from '../assets/NGYU.mp4';
 
+import { useRef, useState } from 'react';
+
 export function Mediaplayer() {
+
+  const videoRef = useRef(null);
+
   return (
-    <div className={styles.mediaplayer}>
+    <div ref={videoRef} className={styles.mediaplayer}>
       <video className={styles.video} src={video1} controls></video>
     </div>
   );
